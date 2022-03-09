5835e25936e0bd190bf84baf6f0979c0db592d5a
Torie Zhang <achangnim@gmail.com>
2022-03-09 11:10:17 +0800

    Build the domain-price component.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+var domaintag = document.querySelectorAll(".domain__tag");
+
+
+
+for (var i = 0;i < domaintag.length; i++) {
+  domaintag[i].onclick = function() {
+    for (var i = 0;i < domaintag.length; i++) {
+      domaintag[i].classList.remove("badge","badge--secondary");
+    } 
+    this.classList.add("badge","badge--secondary");
+  }
+}
+
+// domaintag.forEach((item) =>
+//   item.addEventListener("click", function () {
+//     this.classList.add("badge","badge--secondary");
+//   })
+// );
